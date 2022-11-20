ba77af441cef4a78735b7e4827a7b0bf71a34945
Kirill Cherepanov <kirill2005910@gmail.com>
2022-11-20 17:04:44 +0300

    112296 Самая длинная цепочка

--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int first = Integer.MAX_VALUE;
-        int fcount = 1;
-        int second = Integer.MAX_VALUE;
-        int scount = 1;
-        int third = Integer.MAX_VALUE;
+        int max_number;
+        int counter = 1;
+        int max_counter = 1;
         int[] array = new int[n];
         for(int i = 0; i < n; i++) {
             array[i] = in.nextInt();
         }
-        for(int i = 0; i < n; i++) {
-            if(array[i] < first) {
-                fcount = 1;
-                first = array[i];
-            }
-            if(array[i] == first) {
-                fcount++;
-            }
-        }
-        for(int i = 0; i < n; i++) {
-            if(array[i] < second && array[i] != first) {
-                scount = 1;
-                second = array[i];
-            }
-            if(array[i] == second) {
-                scount++;
-            }
-        }
-        for(int i = 0; i < n; i++) {
-            if(array[i] < third && array[i] != second && array[i] != first) {
-                third = array[i];
-            }
-        }
-        for(int i = 0; i < 3; i++) {
-            if(fcount != 0) {
-                System.out.println(first + " ");
-                fcount--;
-            } else if(scount != 0) {
-                System.out.println(second + " ");
-                scount--;
+        max_number = array[0];
+        for(int i = 0; i < n - 1; i++) {
+            if(array[i] == array[i + 1]) {
+                counter++;
+                if(counter > max_counter) {
+                    max_counter = counter;
+                    max_number = array[i];
+                }
             } else {
-                System.out.println(third);
+                counter = 1;
             }
         }
+        System.out.println(max_number + " " + max_counter);
         in.close();
     }
 }
