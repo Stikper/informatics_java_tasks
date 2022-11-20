ef9fddfe727827e3d6eb7db2c27e9c678947a4fc
Kirill Cherepanov <kirill2005910@gmail.com>
2022-11-20 17:03:43 +0300

    112294 Три минимума

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,49 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int maxcounter = 0;
+        int first = Integer.MAX_VALUE;
+        int fcount = 1;
+        int second = Integer.MAX_VALUE;
+        int scount = 1;
+        int third = Integer.MAX_VALUE;
         int[] array = new int[n];
         for(int i = 0; i < n; i++) {
             array[i] = in.nextInt();
         }
-        int max = Integer.MIN_VALUE;
         for(int i = 0; i < n; i++) {
-            if(array[i] > max) {
-                max = array[i];
+            if(array[i] < first) {
+                fcount = 1;
+                first = array[i];
+            }
+            if(array[i] == first) {
+                fcount++;
             }
         }
         for(int i = 0; i < n; i++) {
-            if(array[i] == max) {
-                maxcounter++;
+            if(array[i] < second && array[i] != first) {
+                scount = 1;
+                second = array[i];
+            }
+            if(array[i] == second) {
+                scount++;
             }
         }
-        System.out.println(max + " " + maxcounter);
+        for(int i = 0; i < n; i++) {
+            if(array[i] < third && array[i] != second && array[i] != first) {
+                third = array[i];
+            }
+        }
+        for(int i = 0; i < 3; i++) {
+            if(fcount != 0) {
+                System.out.println(first + " ");
+                fcount--;
+            } else if(scount != 0) {
+                System.out.println(second + " ");
+                scount--;
+            } else {
+                System.out.println(third);
+            }
+        }
         in.close();
     }
 }
