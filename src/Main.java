9df08be07f5771c932e38f36b225a7a587622f2c
Kirill Cherepanov <kirill2005910@gmail.com>
2022-11-20 17:05:37 +0300

    112276 Заполнение числами Фибоначчи

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int max_number;
-        int counter = 1;
-        int max_counter = 1;
-        int[] array = new int[n];
-        for(int i = 0; i < n; i++) {
-            array[i] = in.nextInt();
-        }
-        max_number = array[0];
-        for(int i = 0; i < n - 1; i++) {
-            if(array[i] == array[i + 1]) {
-                counter++;
-                if(counter > max_counter) {
-                    max_counter = counter;
-                    max_number = array[i];
-                }
-            } else {
-                counter = 1;
+        if(n >= 2) {
+            int[] array = new int[n];
+            array[0] = 1;
+            array[1] = 1;
+            for (int i = 2; i < n; i++) {
+                array[i] = array[i - 1] + array[i - 2];
             }
+            for (int i = 0; i < n; i++) {
+                System.out.println(array[i] + " ");
+            }
+        } else if (n == 1) {
+            System.out.println(1);
         }
-        System.out.println(max_number + " " + max_counter);
         in.close();
     }
 }
