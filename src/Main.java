745df49c6484f5ed8d67c143c0f844e504a0429c
Kirill Cherepanov <kirill2005910@gmail.com>
2022-11-20 17:02:02 +0300

    112291 Число максимумов

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
+import static java.lang.Math.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int maxcounter = 0;
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            if(array[i] > max) {
+                max = array[i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(array[i] == max) {
+                maxcounter++;
+            }
+        }
+        System.out.println(max + " " + maxcounter);
+        in.close();
     }
 }
